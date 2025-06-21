94eb4ef69e3892741527c24bdeba78801ae8174b
CSY0N <134310111+CSY0N@users.noreply.github.com>
2025-06-21 15:45:51 +0200

    Add files via upload

---
+++ game_files/GameDataHash.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "security/ObfVar.hpp"
+#include <array>
+
+#pragma pack(push, 8)
+class GameDataHash
+{
+public:
+	bool m_is_japanese_version;
+	std::array<rage::Obf32, 16> m_data;
+	char new_0104[0x18]; // Obf64, added b3570
+};
+static_assert(sizeof(GameDataHash) == 0x11C);
+#pragma pack(pop)
